fcb37128a9529acc2b592a5b879da58f3fbd3119
catwalter <crw335@drexel.edu>
2021-10-06 00:51:02 -0400

    played with animations
    go to office hours friday to talk to josh about animation issues

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,4 +2,6 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("hero",{duration:2,alpha:0});
+mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
+
+mainTL.from("content-p",{duration:6,alpha:0,scale:3})
