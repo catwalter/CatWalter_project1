536fcd3acc10ba51d77c4fdebc96ed5195317cac
catwalter <crw335@drexel.edu>
2021-10-08 08:09:28 -0400

    fireside chat

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,5 +3,6 @@ import { gsap } from "gsap";
 const mainTL = gsap.timeline();
 
 mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
+.from("#hero h1",{duration:1, y:+-500, rotation:180})
 
-mainTL.from("content-p",{duration:6,alpha:0,scale:3})
+mainTL.from("#content p",{duration:6,alpha:0,scale:3})
