a9bf1c59c2e18325320c60666e924041872fd54f
catwalter <crw335@drexel.edu>
2021-11-07 21:53:03 -0500

    greenstock paid stuff in

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,7 +2,34 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
-.from("#hero h1",{duration:1, y:+-500, rotation:180})
+function paintbrushZigZag(){
 
-mainTL.from("#content p",{duration:6,alpha:0,scale:3})
+    const tl = gsap.timeline();
+    tl.to("#painting-bullseye", {duration:2, scale:3})
+    return tl;
+
+}
+function cottage(){
+
+    const tl = gsap.timeline();
+    tl.to("#cottage", {duration:2, scale:3})
+    return tl;
+
+}
+
+
+mainTL.add(paintbrushZigZag())
+        .add(cottage())
+
+
+
+//mainTL.to("#painting-bullseye", {duration:2, scale:3, y:"-=600"})
+
+
+
+
+
+//mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
+//.from("#hero h1",{duration:1, y:+-500, rotation:180})
+
+//mainTL.from("#content p",{duration:6,alpha:0,scale:3})
