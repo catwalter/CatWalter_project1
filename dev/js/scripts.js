d9065e22cfd6202b21236a0d55c0e0aa0c9d7370
catwalter <crw335@drexel.edu>
2021-11-15 00:13:42 -0500

    animation and finishing touches

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,21 +5,62 @@ gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
-function FadeIn(){
-    const tl = gsap.timeline()
-    tl.from("#painting-bullseye", {duration: 2.5, scaleX: 0})
-    tl.from("#painting-2", {duration: 2.5, scaleX: 0}) 
-    tl.from("#cottage", {duration: 2.5, scaleX: 0})
-    tl.from("#black-and-white-tree", {duration: 2.5, scaleX: 0})
-    tl.from("#painting-5", {duration: 2.5, scaleX: 0})
-    
 
+function Paintings(){
+    const tl = gsap.timeline()
+    tl.from("#frame_1", {duration: .5, alpha: 0})
+    tl.from("#red", {duration: 1, scaleX: 0})
+    tl.from("#Ellipse-5", {duration: .5, alpha: 0})
+    tl.from("#Ellipse-4", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#Ellipse-3", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#Ellipse-2", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#Ellipse-1", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#frame_2", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#background", {duration: 1, scaleY: 0}) 
+    tl.from("#Vector-22", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-21", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-20", {duration: .25, scaleY: 0}, "-=50%")
+    tl.from("#Vector-19", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-18", {duration: .25, scaleY: 0}, "-=50%")
+    tl.from("#Vector-17", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-16", {duration: .25, scaleY: 0}, "-=50%")
+    tl.from("#Vector-15", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Ellipse-6", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#frame_3", {duration: .5, alpha: 0})
+    tl.from("#Vector-13", {duration: .5, scaleX: 0}, "-=50%")
+    tl.from("#Vector-12", {duration: .5, scaleY: 0}, "-=50%")
+    tl.from("#Vector-11", {duration: .5, scaleX: 0}, "-=50%")
+    tl.from("#Vector-14", {duration: .5, scaleY: 0}, "-=50%")
+    tl.from("#Vector-9", {duration: .5, scaleX: 0}, "-=50%")
+    tl.from("#Vector-8", {duration: .5, scaleY: 0}, "-=50%")
+    tl.from("#Vector-10", {duration: .5, scaleX: 0}, "-=50%")
+    tl.from("#frame_4", {duration: .5, alpha: 0})
+    tl.from("#black", {duration: 1.5, scaleX: 0})
+    tl.from("#Vector-7", {duration: .5, alpha: 0})
+    tl.from("#trees", {duration: .5, scaleY: 0}, "-=50%")
+    tl.from("#frame_5", {duration: .5, alpha: 0}, "-=50%")
+    tl.from("#gray-cream", {duration: 1.5, scaleX: 0})
+    tl.from("#Vector-31", {duration: .25, scaleX: 0})
+    tl.from("#Vector-30", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-29", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-28", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-27", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-26", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-25", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Vector-24", {duration: .25, scaleX: 0}, "-=50%")
+    tl.from("#Ellipse-7", {duration: .5, alpha: 0}, "-=50%")
+    .to("#preloader", {duration:1, alpha: 0, onComplete:backToTop})
+    
+    
     return tl;
 }
 
+function backToTop(){
+    window.scrollTo(0,0);
+    gsap.set("#preloader", {display:"none"})
+}
 
-
-mainTL.add(FadeIn())
+mainTL.add(Paintings())
 
 
 
@@ -27,13 +68,13 @@ mainTL.add(FadeIn())
 
 
 GSDevTools.create();
-//mainTL.to("#painting-bullseye", {duration:2, scale:3, y:"-=600"})
 
 
 
 
+mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
+.from("#h1hero",{duration:1, y:+-500, rotation:180})
+.from("#h2hero", {duration:2, y:+-500, scale:4})
 
-//mainTL.from("#hero",{duration:4,alpha:0,scale:5,y:600})
-//.from("#hero h1",{duration:1, y:+-500, rotation:180})
-
-//mainTL.from("#content p",{duration:6,alpha:0,scale:3})
+mainTL.from("#title-content",{duration:4,alpha:0,scale:3})
+mainTL.from("#content-p",{duration:2,alpha:0,rotation: 180}, "-=50%")
